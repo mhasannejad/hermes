850fcaa3405beb70b67b1fd8f7b6c7290b477fc2
drgeek <mngmnt.hasannejad@gmail.com>
2022-09-23 15:49:11 +0330

    static files

--- neo_hermes/settings.py
+++ neo_hermes/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.gethermes.ir'
 EMAIL_HOST_USER = 'notification@gethermes.ir'

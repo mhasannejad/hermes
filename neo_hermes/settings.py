16efd724d75ba7b74b5bbcb7b176681d9f12b229
drgeek <mngmnt.hasannejad@gmail.com>
2022-09-23 16:02:03 +0330

    debug mode changed

--- neo_hermes/settings.py
+++ neo_hermes/settings.py
@@ -26,7 +26,10 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if env('MODE') == 'DEBUG':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
